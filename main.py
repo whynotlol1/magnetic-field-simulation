8ac471e9aee6e796d2665b80d0b04d4a971ca1d3
cat dev <117660464+whynotlol1@users.noreply.github.com>
2023-02-24 00:35:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 import scipy.integrate as integrate
 
 pygame.init()
-height = width = 800
+standart = height = width = 800
 screen = pygame.display.set_mode((height, width))
 pygame.display.set_caption("magnetic field simulation")
 pygame.font.init()
@@ -14,14 +14,14 @@ magnet1 = pygame.Surface((magnet_standart, magnet_standart))
 magnet1_data = {
     "coords": [375, 375],
     "moving_speed": 0.3,
-    "strength": 0.53 # strength parameter must be greater or equal to 0.35
+    "strength": 0.53  # strength parameter must be greater or equal to 0.35
 }
 
 magnet2 = pygame.Surface((magnet_standart, magnet_standart))
 magnet2_data = {
     "coords": [375, 375],
     "moving_speed": 0.3,
-    "strength": 0.53 # strength parameter must be greater or equal to 0.35
+    "strength": 0.53  # strength parameter must be greater or equal to 0.35
 }
 
 if __name__ == '__main__':
@@ -44,8 +44,6 @@ if __name__ == '__main__':
         pygame.draw.circle(screen, (255, 0, 255), (magnet2_data["coords"][0], magnet2_data["coords"][1]), magnetic_field_line_radius / 2, 2)
         pygame.draw.circle(screen, (255, 0, 255), (magnet2_data["coords"][0], magnet2_data["coords"][1]), magnetic_field_line_radius / 1.5, 2)
         pygame.draw.circle(screen, (255, 0, 255), (magnet2_data["coords"][0], magnet2_data["coords"][1]), magnetic_field_line_radius, 2)
-
-
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP] and magnet1_data["coords"][1] > 0 + magnet_standart / 2:
@@ -79,7 +77,6 @@ if __name__ == '__main__':
         if keys[pygame.K_4]:
             magnet2_data["strength"] = magnet2_data["strength"] - 0.01 if magnet2_data["strength"] > 0.35 else magnet2_data["strength"]
             time.sleep(0.1)
-
 
         pygame.display.update()
 
